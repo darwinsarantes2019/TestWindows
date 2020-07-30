2dd9b2e606560ca8fffdefd206475451e0221de8
JusetFT <68977463+JusetFT@users.noreply.github.com>
2020-07-29 19:34:08 -0600

    Cambio formulario para test

--- TestWindows/Form1.Designer.cs
+++ TestWindows/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Sarantesnoseavestia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Sarantesnoseavestia
+            // 
+            this.Sarantesnoseavestia.Location = new System.Drawing.Point(447, 111);
+            this.Sarantesnoseavestia.Name = "Sarantesnoseavestia";
+            this.Sarantesnoseavestia.Size = new System.Drawing.Size(186, 23);
+            this.Sarantesnoseavestia.TabIndex = 0;
+            this.Sarantesnoseavestia.Text = "Sarantes";
+            this.Sarantesnoseavestia.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 261);
+            this.Controls.Add(this.Sarantesnoseavestia);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Sarantesnoseavestia;
     }
 }
 
